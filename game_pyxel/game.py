395994dc2f2e5a473f4ebe1ca17a705dac54c3c8
ito_t <st25019tt@gm.ibaraki-ct.ac.jp>
2025-09-28 02:14:20 +0900

    ゲーム体験(最初だけ)

---
+++ game_pyxel/game.py
@@ -0,0 +1,25 @@
+import pyxel
+
+class App:
+    def __init__(self):
+        pyxel.init(160,120,title="チーげーみゅ")
+        pyxel.mouse(True)
+        self.number = 0
+        pyxel.run(self.update, self.draw)
+        
+    def update(self):
+        if pyxel.btnp(pyxel.KEY_ESCAPE):
+            pyxel.quit()
+        if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
+            if 20 <= pyxel.mouse_x <= 40 and 50<= pyxel.mouse_y <= 70:
+                self.number -= 1
+            elif 100 <= pyxel.mouse_x <= 120 and 50 <= pyxel.mouse_y <= 70:
+                self.number += 1
+    
+    def draw(self):
+        pyxel.cls(pyxel.COLOR_DARK_BLUE)
+        pyxel.text(70, 60, f"{self.number}", pyxel.COLOR_WHITE)
+        pyxel.text(30, 60, "-", pyxel.COLOR_LIME)
+        pyxel.text(110, 60, "+", pyxel.COLOR_LIME)
+        
+App()
